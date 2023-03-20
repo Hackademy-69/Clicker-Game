d5fc3ccff7f69d8639b9266e17a19ba233c17ec2
AyatoVevo <axel.yt.437@gmail.com>
2023-03-20 15:05:49 +0100

    test localstorage

--- main.js
+++ main.js
@@ -8,42 +8,73 @@ let clickCost = document.querySelector('#clickCost')
 let clickBtn = document.querySelector('#clickBtn')
 
 
-// variabili di partenza
-let coins = 0 
-let upgradeCost = 10
-let clickPower = 1
+
+// coin data 
+let coins;
+
+localStorage.setItem('Coins', coins)
+if (localStorage.getItem('Coins')) {
+    coins = 0;
+    localStorage.getItem('Coins', coins) 
+   
+  }else {
+    localStorage.setItem('Coins', 0);
+    coins = paresInt(localStorage.getItem('Coins'));
+  }
+// click power data
+let clickPower;
+localStorage.setItem('clickPower', clickPower) 
+if (localStorage.getItem('clickPower')) {
+    clickPower = 1;
+    localStorage.getItem('clickPower', clickPower) 
+   
+  }else {
+    localStorage.setItem('clickPower', 1);
+    clickPower = paresInt(localStorage.getItem('clickPower'));
+  }
+
+
+let upgradeCost;
+localStorage.setItem('upgradeCost', upgradeCost) 
+if (localStorage.getItem('upgradeCost')) {
+    upgradeCost = 10;
+    localStorage.getItem('upgradeCost', upgradeCost) 
+    
+  }else {
+    localStorage.setItem('upgradeCost', 10);
+    upgradeCost = paresInt(localStorage.getItem('upgradeCost'));
+  }
 
 
 // click per incremento monete + aggiornamento testo
 leaf.addEventListener('click', ()=>{
     
-    clickCounter.innerHTML = coins += clickPower
+
     
+    clickCounter.innerHTML = coins += clickPower;
+
+    localStorage.setItem("Coins", coins);
+    localStorage.setItem('ClickPower', clickPower);
 })
 
-// bottone per potenziare il click
 upgradeBtn.addEventListener('click', ()=>{
-
-    // se non hai monete abbastanza il codice non parte
+    localStorage.getItem('Coins', coins);
     if(coins >= upgradeCost){
 
-        // mi toglie monete in base al costo
-        clickCounter.innerHTML = coins -= upgradeCost
+        clickCounter.innerHTML = coins -= upgradeCost;
+        upgradeCost = Math.round(upgradeCost * 1.73);
+        clickCost.innerHTML = "Cost: " +  upgradeCost;
+        clickPower = Math.ceil(clickPower * 1.4);
+        clickBtn.innerHTML = 'Power : ' + clickPower;
 
-        // formula per incrementare costo ogni livello
-        upgradeCost = Math.round(upgradeCost * 1.73)
-
-        // aggiornamento testo
-        clickCost.innerHTML = "Cost: " +  upgradeCost
-        
-        // formula per incrementale il potere di click 
-        clickPower = Math.ceil(clickPower * 1.4)
-
-        // aggiornamento testo 
-        clickBtn.innerHTML = 'Power : ' + clickPower
+        localStorage.setItem('upgradeCost', upgradeCost);
+        localStorage.setItem('clickPower', clickPower);
     }
     
     
     
     
 })
+
+
+
